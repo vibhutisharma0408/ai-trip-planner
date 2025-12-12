0760c8e82031db6c99c5b36fccdcc6915b05ad97
vibhuti sharma <aachalmsmd@gmail.com>
2025-12-13 01:30:54 +0530

    chore(build): standardize on npm (remove yarn.lock), disable Sentry, set Turbopack config

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,13 +1,5 @@
-import * as Sentry from '@sentry/nextjs';
-
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./sentry.server.config');
-  }
-
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./sentry.edge.config');
-  }
+  // No-op instrumentation since Sentry is disabled
 }
 
-export const onRequestError = Sentry.captureRequestError;
+export const onRequestError = () => {};
